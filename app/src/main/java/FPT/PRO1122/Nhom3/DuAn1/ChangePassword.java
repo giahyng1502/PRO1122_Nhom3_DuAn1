1fbc6beb99b698fbcedf418e0e901196b126b703
movetime2003 <movetime3007@gmail.com>
2024-07-19 10:32:10 +0700

    update 1

---
+++ app/src/main/java/FPT/PRO1122/Nhom3/DuAn1/ChangePassword.java
@@ -0,0 +1,35 @@
+package FPT.PRO1122.Nhom3.DuAn1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ChangePassword extends AppCompatActivity {
+    ImageView btn_back;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_change_password);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //1
+        btn_back=findViewById(R.id.btn_back_changepass);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/FPT/PRO1122/Nhom3/DuAn1/Fragment/Profile.java
+++ app/src/main/java/FPT/PRO1122/Nhom3/DuAn1/Fragment/Profile.java
@@ -1,5 +1,6 @@
 package FPT.PRO1122.Nhom3.DuAn1.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,11 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import FPT.PRO1122.Nhom3.DuAn1.ChangePassword;
+import FPT.PRO1122.Nhom3.DuAn1.DeliveryAddress;
+import FPT.PRO1122.Nhom3.DuAn1.EditProfile;
+import FPT.PRO1122.Nhom3.DuAn1.OrderHistory;
+import FPT.PRO1122.Nhom3.DuAn1.Payment;
 import FPT.PRO1122.Nhom3.DuAn1.R;
 
 public class Profile extends Fragment {
-
+    LinearLayout payment_layout,order_layout,changepass_layout,delivery_layout;
+    Button btn_logout,btn_editprofile;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +31,73 @@ public class Profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view=inflater.inflate(R.layout.fragment_profile, container, false);
+        //anh xa
+        payment_layout=view.findViewById(R.id.payment_layout);
+        order_layout=view.findViewById(R.id.orderhistory_layout);
+        delivery_layout=view.findViewById(R.id.delivery_layout);
+        changepass_layout=view.findViewById(R.id.changepass_layout);
+        btn_logout=view.findViewById(R.id.btn_logout);
+        btn_editprofile=view.findViewById(R.id.btn_editprofile);
+        //
+
+        //payment
+        payment_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), Payment.class);
+                startActivity(intent);
+            }
+        });
+
+        //order
+        order_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), OrderHistory.class);
+                startActivity(intent);
+            }
+        });
+
+        //delivery address
+        delivery_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), DeliveryAddress.class);
+                startActivity(intent);
+            }
+        });
+
+        //changepass
+        changepass_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), ChangePassword.class);
+                startActivity(intent);
+            }
+        });
+
+        //edit profile
+        btn_editprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        //logout
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
+        //
+
     }
+    //
+
 }
