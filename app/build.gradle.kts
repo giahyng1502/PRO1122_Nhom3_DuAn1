8859215fd3a721358558b097bc2b8372f9d33f57
Hoang Van Hung <hungcy1502@gmail.com>
2024-07-31 00:11:02 +0700

    Merge branch thienhdph47232 fix category

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ android {
 dependencies {
     // Glide
     implementation ("com.github.bumptech.glide:glide:4.12.0")
+    implementation(libs.firebase.firestore)
     annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")
 
     // Google Material
--- app/src/main/java/FPT/PRO1122/Nhom3/DuAn1/Activity/CartActivity.java
+++ app/src/main/java/FPT/PRO1122/Nhom3/DuAn1/Activity/CartActivity.java
@@ -1,19 +1,22 @@
 package FPT.PRO1122.Nhom3.DuAn1.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 import FPT.PRO1122.Nhom3.DuAn1.DAO.QuanLyGioHang;
-import FPT.PRO1122.Nhom3.DuAn1.R;
+import FPT.PRO1122.Nhom3.DuAn1.model.GioHang;
 import FPT.PRO1122.Nhom3.DuAn1.adapter.GioHangAdapter;
 import FPT.PRO1122.Nhom3.DuAn1.databinding.ActivityCartBinding;
 
@@ -22,49 +25,67 @@ public class CartActivity extends BaseActivity {
     private RecyclerView.Adapter adapter;
     private QuanLyGioHang quanLyGioHang;
     private  double tax;
+    Context context;
+
+    private ArrayList<GioHang> cartList = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityCartBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        quanLyGioHang = new QuanLyGioHang(this);
+        quanLyGioHang = new QuanLyGioHang(this, MainActivity.id);
 
         setVariable();
-        caculataCart();
+        caculateCart();
         initList();
         binding.backBtn.setOnClickListener(v-> startActivity(new Intent(
                 CartActivity.this,MainActivity.class)));
     }
 
     private void initList() {
-        if (quanLyGioHang.getListCart().isEmpty()){
-            binding.emptyTxt.setVisibility(View.VISIBLE);
-            binding.scrollviewCart.setVisibility(View.GONE);
-        }else{
-            binding.emptyTxt.setVisibility(View.GONE);
-            binding.scrollviewCart.setVisibility(View.VISIBLE);
-        }
+        DatabaseReference myRef = database.getReference("Carts").child(MainActivity.id);
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    cartList.clear();
+                    for (DataSnapshot issue : snapshot.getChildren()) {
+                        GioHang gioHang = issue.getValue(GioHang.class);
+                        cartList.add(gioHang);
+                    }
+                    if (!cartList.isEmpty()) {
+                        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(CartActivity.this, LinearLayoutManager.VERTICAL, false);
+                        binding.cartRec.setLayoutManager(linearLayoutManager);
+                        adapter = new GioHangAdapter(cartList, context, () -> caculateCart());
+                        binding.cartRec.setAdapter(adapter);
+                    }
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        binding.cartRec.setLayoutManager(linearLayoutManager);
-        adapter = new GioHangAdapter(quanLyGioHang.getListCart(), this, () -> caculataCart());
+                }
+            }
 
-        binding.cartRec.setAdapter(adapter);
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
-    private void caculataCart() {
+    private void caculateCart() {
         double percentTax = 0.08;
         double delivery = 10000;
+        double totalFee = 0;
 
-        tax = Math.round((quanLyGioHang.getTotalFee() * percentTax) * 100) / 100;
-        double total = Math.round((quanLyGioHang.getTotalFee() + tax + delivery) * 100) / 100;
-        double itemTotal = Math.round(quanLyGioHang.getTotalFee() * 100) / 100;
-
-        binding.totalFeeTxt.setText(itemTotal + " VND");
-        binding.taxTxt.setText(tax + " VND");
-        binding.deliveryFeeTxt.setText(delivery + " VND");
-        binding.totalTxt.setText(total + " VND");
+//        tax = Math.round((quanLyGioHang.getTotalFee() * percentTax) * 100) / 100;
+//        double total = Math.round((quanLyGioHang.getTotalFee() + tax + delivery) * 100) / 100;
+//        double itemTotal = Math.round(quanLyGioHang.getTotalFee() * 100) / 100;
+//
+//        binding.totalFeeTxt.setText(itemTotal + " VND");
+//        binding.taxTxt.setText(tax + " VND");
+//        binding.deliveryFeeTxt.setText(delivery + " VND");
+//        binding.totalTxt.setText(total + " VND");
     }
 
     private void setVariable() {
